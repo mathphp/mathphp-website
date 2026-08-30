6f07c306b7af32b8c03353c678c13512bde31d60
Mark Topper Diderichsen <mdd@eupry.com>
2026-08-30 11:03:59 +0200

    Fix authenticated Units clone

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,11 +8,7 @@ ref="${MATHPHP_UNITS_REF:-main}"
 
 if [ -n "$token" ] && [ ! -f "$private_dir/vendor/autoload.php" ]; then
     mkdir -p /app/private
-    export GIT_CONFIG_COUNT=1
-    export GIT_CONFIG_KEY_0=http.https://github.com/.extraheader
-    export GIT_CONFIG_VALUE_0="Authorization: Bearer $token"
-    git clone --depth 1 --branch "$ref" https://github.com/mathphp/mathphp-units.git "$private_dir"
-    unset GIT_CONFIG_COUNT GIT_CONFIG_KEY_0 GIT_CONFIG_VALUE_0
+    git -c "http.extraHeader=Authorization: Bearer $token" clone --depth 1 --branch "$ref" https://github.com/mathphp/mathphp-units.git "$private_dir"
     composer install --working-dir="$private_dir" --no-dev --prefer-dist --no-interaction --no-progress --optimize-autoloader
 fi
 
