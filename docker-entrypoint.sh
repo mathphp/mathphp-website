c20452c6fef473ddcc1c3d101c14c01808b232d6
Mark Topper Diderichsen <mdd@eupry.com>
2026-09-01 11:26:02 +0200

    Pin website to numerical primitive releases

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,8 @@
 set -eu
 
 token="${MATHPHP_PRIVATE_REPO_TOKEN:-${MATHPHP_UNITS_REPO_TOKEN:-}}"
-default_ref="${MATHPHP_PRIVATE_REPO_REF:-v0.3.5}"
-explaining_ref="${MATHPHP_EXPLAINING_REF:-v0.27.0}"
+default_ref="${MATHPHP_PRIVATE_REPO_REF:-v0.3.6}"
+explaining_ref="${MATHPHP_EXPLAINING_REF:-v0.27.1}"
 
 install_private_package() {
     package="$1"
