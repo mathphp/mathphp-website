812317285ab09902a9a2b42deb0743fe09ad67e6
Mark Topper Diderichsen <mdd@eupry.com>
2026-09-01 10:36:15 +0200

    Align website with Unicode notation releases

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,8 @@
 set -eu
 
 token="${MATHPHP_PRIVATE_REPO_TOKEN:-${MATHPHP_UNITS_REPO_TOKEN:-}}"
-default_ref="${MATHPHP_PRIVATE_REPO_REF:-v0.3.0}"
-explaining_ref="${MATHPHP_EXPLAINING_REF:-v0.22.0}"
+default_ref="${MATHPHP_PRIVATE_REPO_REF:-v0.3.1}"
+explaining_ref="${MATHPHP_EXPLAINING_REF:-v0.22.1}"
 
 install_private_package() {
     package="$1"
